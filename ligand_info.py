7b2a03bb3585c58b647d0c8eb9801428de22813a
YedaAnna <abhiramgadekar009@gmail.com>
2014-12-24 13:03:15 +0530

    Update ligand_info.py

--- ligand_info.py
+++ ligand_info.py
@@ -14,23 +14,22 @@ for index in molecule_list:
 	resolution=0
 	ligand_file= open(index, 'r')
 	for line in ligand_file:
-    column = line.split()
-    if column[0] == "HET":
-      ligand.append(column[1])
-      chain.append(column[2])
-    if column[0] == "HETNAM":
-      	if column[1].isdigit()==False:
-      		lig_name.append(column[2])
-    if column[0]=='REMARK':
-					if column[1]=='2':
-						if len(column)>2:
-							if column[2] =='RESOLUTION.':
-								resolution=column[3]	
-          
+		column = line.split()
+    		if column[0] == "HET":
+      			ligand.append(column[1])
+      			chain.append(column[2])
+    		if column[0] == "HETNAM":
+      			if column[1].isdigit()==False:
+      				lig_name.append(column[2])
+    		if column[0]=='REMARK':
+			if column[1]=='2':
+				if len(column)>2:
+					if column[2] =='RESOLUTION.':
+						resolution=column[3]	
 	print "RESOLUTION of pdb is " + str(resolution) + " Angstroms"                
 	print "list of ligands present:" + str(ligand)
 	print "corresponding chains are:" + str(chain)
 	print "Ligand name is " + str(lig_name)
 	ligand_file.close()
 	print "End of molecule: " + os.path.basename(index) +"\n\n"
-print "Endof program"
+print "End of program"
