bd338d4caa255be2a139587fca59e3f761f8a203
YedaAnna <abhiramgadekar009@gmail.com>
2015-01-06 18:01:59 +0530

    Completely improved code

--- ligand_info.py
+++ ligand_info.py
@@ -1,36 +1,76 @@
 import os
-import sys
 import glob
+from collections import OrderedDict
+import time
 global ligand
-molecule_list=[]
-molecule_list=glob.glob(os.getcwd() +'/*.pdb')# read all the molecules in a directory and adds them to a list
+start_time = time.time()
+
+
+def HET():
+    global unique_no, unique, pdb_file
+    unique_no = 0
+    pdb_file = open(index, 'r')
+    for line in pdb_file:
+        column = line.split()
+        if column[0] == "HET":
+            ligand.append(column[1])
+            chain.append(column[2])
+    print "list of ligands present:" + str(ligand)
+    print "corresponding chains are:" + str(chain)
+    unique = list(OrderedDict.fromkeys(ligand))
+    unique_no = len(unique)
+    print "List of unique ligands are : " + str(unique)
+
+
+def RESOLUTION():
+    global resolution
+    resolution = 0
+    pdb_file = open(index, 'r')
+    for line in pdb_file:
+        column = line.split()
+        if column[0] == 'REMARK':
+            if column[1] == '2':
+                if len(column) > 2:
+                    if column[2] == 'RESOLUTION.':
+                        resolution = column[3]
+    print "RESOLUTION of pdb is " + str(resolution) + " Angstroms"
+
+
+def lig():
+    pdb_file = open(index, 'r')
+    for line in pdb_file:
+        column = line.split()
+        if column[0] == 'HETNAM':
+            if column[1] in unique:
+                global temp
+                joined = ' '.join(column[2:])
+                lig_name.append(joined)
+                temp = column[2]
+            else:
+                if column[1] == '3':
+                    continue
+                lig_name.remove(temp)
+                joined2 = ' '.join(column[3:])
+                temp2 = temp + joined2
+                lig_name.append(temp2)
+
+    print "Corresponding ligand name is : " + str(lig_name)
+
+
+def basic_info():
+    HET()
+    lig()
+    RESOLUTION()
+
+molecule_list = []
+# read all the molecules in a directory and adds them to a list
+molecule_list = glob.glob(os.getcwd() + '/*.pdb')
 for index in molecule_list:
-	print "start of molecule: " + os.path.basename(index)
-	ligand=[]
-	chain=[]
-	lig_name=[]
-	resolution=0
-	ligand_file= open(index, 'r')
-	for line in ligand_file:
-		column = line.split()
-    		if column[0] == "HET":
-      			ligand.append(column[1])
-      			chain.append(column[2])
-    		if column[0] == "HETNAM":
-			if column[1].isdigit()==False:
-				name_part = column[2]			
-			else:
-				name_part_two = column[3]
-				lig_name.append(name_part+name_part_two)
-    		if column[0]=='REMARK':
-			if column[1]=='2':
-				if len(column)>2:
-					if column[2] =='RESOLUTION.':
-						resolution=column[3]	
-	print "RESOLUTION of pdb is " + str(resolution) + " Angstroms"                
-	print "list of ligands present:" + str(ligand)
-	print "corresponding chains are:" + str(chain)
-	print "Ligand name is " + str(lig_name)
-	ligand_file.close()
-	print "End of molecule: " + os.path.basename(index) +"\n\n"
+    print "start of molecule: " + os.path.basename(index)
+    ligand = []
+    chain = []
+    lig_name = []
+    basic_info()
+    print "End of molecule: " + os.path.basename(index) + "\n\n"
 print "End of program"
+print "time taken: " + str(time.time()-start_time) + "seconds"
